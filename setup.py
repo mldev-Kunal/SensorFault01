cb7ff2dcbc26c8079f7864ba6a3d9f5da09ef10a
Kunal Patel <tech02042001@gmail.com>
2025-12-08 23:49:32 +0530

    my first commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+exit
+from setuptools import setup, find_packages
+from typing import List
+
+
+def get_requirements(file_path: str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.strip() for req in requirements if req.strip()]
+    return requirements        
+
+setup(
+    name="SensorFaultDetection",
+    version="0.0.1",
+    author="Kunal",
+    author_email="tech02042001@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt")
+)
